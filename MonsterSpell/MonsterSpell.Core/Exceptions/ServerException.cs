f7f3b0166c6d1fa7c94fbee6ba44e28014ed3c55
Alexandur Seferinkyn <d0ntth1nc@gmail.com>
2015-02-04 23:50:42 +0200

    login/register implemented

---
+++ MonsterSpell/MonsterSpell.Core/Exceptions/ServerException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MonsterSpell.Core.Exceptions
+{
+    public class ServerException : ApplicationException
+    {
+        public ServerException(string msg)
+            : base(msg) { }
+
+        public ServerException(string msg, Exception inner)
+            : base(msg, inner) { }
+    }
+}
