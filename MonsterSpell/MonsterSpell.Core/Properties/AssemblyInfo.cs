ff46a1597a555523252229046bb0d6ebdf1156b6
Alexandur Seferinkyn <d0ntth1nc@gmail.com>
2015-02-04 12:33:24 +0200

    added class library project

--- MonsterSpell/MonsterSpell.Core/Properties/AssemblyInfo.cs
+++ MonsterSpell/MonsterSpell.Core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e274c9a3-2223-49fa-9148-94327d183672")]
+[assembly: Guid("211c1e89-f091-4153-bf94-845ba8e37f54")]
 
 // Version information for an assembly consists of the following four values:
 //
