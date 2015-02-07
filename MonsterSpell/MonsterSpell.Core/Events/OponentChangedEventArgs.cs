869d58e380da08241c545690062cc9c398d946b2
Alexandur Seferinkyn <d0ntth1nc@gmail.com>
2015-02-07 03:52:13 +0200

    missing files

---
+++ MonsterSpell/MonsterSpell.Core/Events/OponentChangedEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using MonsterSpell.Core.Players;
+using System;
+
+namespace MonsterSpell.Core.Events
+{
+    public class OponentChangedEventArgs : EventArgs
+    {
+        public OponentChangedEventArgs(IPlayer opponent)
+        {
+            this.Opponent = opponent;
+        }
+
+        public IPlayer Opponent { get; private set; }
+    }
+}
